a343d7cff11547a4b1a9936fba152ea0cbb5311e
Angel Hristov <angelxristov84@abv.bg>
2019-07-12 22:44:10 +0300

    try - catch block added

--- AnimalFarm/AnimalFarm/Program.cs
+++ AnimalFarm/AnimalFarm/Program.cs
@@ -7,13 +7,21 @@
     {
         public static void Main(string[] args)
         {
-            string name = Console.ReadLine();
-            int age = int.Parse(Console.ReadLine());
+            try
+            {
+                string name = Console.ReadLine();
+                int age = int.Parse(Console.ReadLine());
 
-            Chicken chicken = new Chicken(name, age);
+                Chicken chicken = new Chicken(name, age);
 
-            Console.WriteLine($"Chicken {chicken.Name} (age {chicken.Age})" +
-                $" can produce {chicken.ProductPerDay} eggs per day.");
+                Console.WriteLine($"Chicken {chicken.Name} (age {chicken.Age})" +
+                    $" can produce {chicken.ProductPerDay} eggs per day.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
         }
     }
 }
